0e8e865c10485282d946557b02887bd2c79a708e
Thundercloudcat <31827741+Thundercloudcat@users.noreply.github.com>
2022-11-05 09:53:06 -0500

    Update SquareCubeandPymaridCalculator.cs

--- SquareCubeandPymaridCalculator.cs
+++ SquareCubeandPymaridCalculator.cs
@@ -1,36 +1,38 @@
-﻿    Console.Write("Square, cube or pymarid? ");
-    string Shape = Console.ReadLine();
-    string ToLowerShape = Shape.ToLower();
-int Length;
-int Width;
-int Height;
+Console.Write("Square, cube or pymarid? ");
+string Shape = Console.ReadLine();
+string ToLowerShape = Shape.ToLower();
+double Length;
+double Width;
+double Height;
 switch (ToLowerShape)
 {
     case "square":
         Console.Write("Length? ");
-        Length = Convert.ToInt32(Console.ReadLine());
+        Length = Convert.ToDouble(Console.ReadLine());
         Console.Write("Width? ");
-        Width = Convert.ToInt32(Console.ReadLine());
+        Width = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine("The area of the square is " + Length * Width);
         break;
 
     case "cube":
         Console.Write("Length? ");
-        Length = Convert.ToInt32(Console.ReadLine());
+        Length = Convert.ToDouble(Console.ReadLine());
         Console.Write("Width? ");
-        Width = Convert.ToInt32(Console.ReadLine());
+        Width = Convert.ToDouble(Console.ReadLine());
         Console.Write("Height? ");
-        Height = Convert.ToInt32(Console.ReadLine());
+        Height = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine("The area of the cube is " + Length * Width * Height);
         break;
 
     case "pymarid":
         Console.Write("Length? ");
-        Length = Convert.ToInt32(Console.ReadLine());
+        Length = Convert.ToDouble(Console.ReadLine());
         Console.Write("Width? ");
-        Width = Convert.ToInt32(Console.ReadLine());
+        Width = Convert.ToDouble(Console.ReadLine());
         Console.Write("Height? ");
-        Height = Convert.ToInt32(Console.ReadLine());
+        Height = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine("The area of the pymarid is " + Length * Width * Height / 2);
+        break;
+        default: Console.WriteLine("Invalid shape!");
         break;
 }
